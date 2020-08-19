0e2f41c503a6ad8671975f477d6a87596719fd08
wujianlun <wujianlun@do1.com.cn>
2020-08-19 14:22:55 +0800

    aaa

--- qiqiao_page/mobile_page/form_components/mb_multiFormAssociation_component.py
+++ qiqiao_page/mobile_page/form_components/mb_multiFormAssociation_component.py
@@ -7,17 +7,25 @@ class MbMultiFormAssociation(SeleniumPage):
 
     MultiFormAssociation_AddButton_loc = "//div[@title='%title']//div[@class='dyFormMultiFormAssociation_add']"  #添加按钮元素
 
+    MultiFormAssociation_rightAddButton_loc = "//div[@title='%title']//span[@class='title_add']" # 添加按钮元素
+
     MultiFormManagementDialog_selected_loc = "//div[@class='el-table__fixed-body-wrapper']//table[@class='el-table__body']//tr[%rowIndex]//td[last()]//span[@class='el-checkbox__inner']"
 
     MultiFormManagementDialog_Button_loc = "//div[contains(@class,'multiFormAssociation_choose_bottom')]//button[contains(text(),'%s')]"
 
     MultiForm_Td_loc = "//div[@title='%s']//table[@class='el-table__body']//tr[%row]/td[%col]//div[@class='text_overflow']"
 
+    MultiForm_Td_order_operation_loc = "//div[@title='%s']//table[@class='el-table__body']//tr[%row]/td[%col]//div[@class='subformTable_order subformTable_order_operation']"
+
     MultiForm_Td_iconDelete_loc = "//div[@title='%s']//i[@class='iconfont icon-del iconDelete']"
 
     def MultiForm_AddButton_Click(self,fileName,*args):
         '''点击添加按钮'''
         self.clickElemByXpath_visibility (self.MultiFormAssociation_AddButton_loc.replace ('%title', fileName))
+
+    def MultiForm_rightAddButton_Click(self,fileName,*args):
+        '''点击右上角添加按钮'''
+        self.clickElemByXpath_visibility (self.MultiFormAssociation_rightAddButton_loc.replace ('%title', fileName))
 
 
     def MultiForm_BathManagePage_Record_Tick(self,fileName,rowIndexList,*args):
@@ -43,6 +51,6 @@ class MbMultiFormAssociation(SeleniumPage):
     def MultiForm_DeletTdValue( self,fileName,row,*args):
         '''删除多表关联组件中间表数据'''
         #点击删除数据的序号
-        self.find_elenmInElemsByXpath_visibility_of_any_elements_located(self.MultiForm_Td_loc.replace('%s',fileName).replace('%row',str(row)).replace('%col',str(1))).click()
+        self.clickElemByXpath_visibility(self.MultiForm_Td_order_operation_loc.replace('%s',fileName).replace('%row',str(row)).replace('%col',str(1)))
         #点击删除按钮
         self.clickElemByXpath_visibility(self.MultiForm_Td_iconDelete_loc.replace('%s',fileName))
