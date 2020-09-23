b1adebb00d5d7915b48d9b6a3e0b7a81913e6ba3
wujianlun <wujianlun@do1.com.cn>
2020-09-23 10:35:29 +0800

    aaa

--- qiqiao_page/mobile_page/form_components/mb_multiFormAssociation_component.py
+++ qiqiao_page/mobile_page/form_components/mb_multiFormAssociation_component.py
@@ -21,16 +21,16 @@ class MbMultiFormAssociation(SeleniumPage):
 
     MultiForm_Td_iconDelete_loc = "//div[@title='%s']//i[@class='iconfont icon-del iconDelete']"
 
-    def MultiForm_AddButton_Click(self,fileName,*args):
+    def MbMultiForm_AddButton_Click(self,fileName,*args):
         '''点击添加按钮'''
         self.clickElemByXpath_visibility (self.MultiFormAssociation_AddButton_loc.replace ('%title', fileName))
 
-    def MultiForm_rightAddButton_Click(self,fileName,*args):
+    def MbMultiForm_rightAddButton_Click(self,fileName,*args):
         '''点击右上角添加按钮'''
         self.clickElemByXpath_visibility (self.MultiFormAssociation_rightAddButton_loc.replace ('%title', fileName))
 
 
-    def MultiForm_BathManagePage_Record_Tick(self,fileName,rowIndexList,*args):
+    def MbMultiForm_BathManagePage_Record_Tick(self,fileName,rowIndexList,*args):
         '''勾选批量管理页面关联表记录'''
         for rowIndex in rowIndexList:
             str_rowIndex = str(rowIndex)
@@ -39,19 +39,19 @@ class MbMultiFormAssociation(SeleniumPage):
             time.sleep(2)
 
 
-    def MultiForm_BathManagePage_Button_Cick(self,fileName,buttonName,*args):
+    def MbMultiForm_BathManagePage_Button_Cick(self,fileName,buttonName,*args):
         '''点击批量管理页面按钮'''
         self.clickElemByXpath_visibility (self.MultiFormManagementDialog_Button_loc.replace ('%s', buttonName))
 
 
-    def MultiForm_GetTdValue( self,fileName,row,col,*args):
+    def MbMultiForm_GetTdValue( self,fileName,row,col,*args):
         '''获取多表关联组件中间表单元格值'''
         text = self.find_elenmInElemsByXpath_visibility_of_any_elements_located(self.MultiForm_Td_loc.replace('%s',fileName).replace('%row',str(row)).replace('%col',str(col))).text
         if(text==""):
             text = self.getElemAttrValue(self.find_elenmInElemsByXpath_visibility_of_any_elements_located(self.MultiForm_Td_loc.replace('%s',fileName).replace('%row',str(row)).replace('%col',str(col))+"//input"),"value")
         return text
 
-    def MultiForm_DeletTdValue( self,fileName,row,*args):
+    def MbMultiForm_DeletTdValue( self,fileName,row,*args):
         '''删除多表关联组件中间表数据'''
         #点击删除数据的序号
         self.clickElemByXpath_visibility(self.MultiForm_Td_order_operation_loc.replace('%s',fileName).replace('%row',str(row)).replace('%col',str(1)))
---
+++ testcase/capital_app/mobile_testcase/test_004.py
@@ -0,0 +1,122 @@
+# coding=utf-8
+import time
+import unittest
+
+from public.driver import Driver
+from qiqiao_page.mobile_page.mb_business_page import MbBusinessPage
+from qiqiao_page.mobile_page.mb_form_page import MbFormPage
+from qiqiao_page.mobile_page.mobile_application_page import MbApplicationListPage
+from qiqiao_page.mobile_page.mobile_home_page import MbHomePage
+from qiqiao_page.mobile_page.mobile_login_page import MbLoginPage
+from qiqiao_page.mobile_page.mobile_to_do_page import MbTodoPage
+from qiqiao_page.pc_page.applicationList_page import ApplicationListPage
+from qiqiao_page.pc_page.business_page import BusinessPage
+from qiqiao_page.pc_page.portal_page import PortalPage
+from qiqiao_page.pc_page.login_page import LoginPage
+
+
+class MbHSEAppTest_003(unittest.TestCase):
+    '''移动端HSE管理流程检查'''
+
+
+    def mbLogin(self,account,password):
+        '''登录移动端'''
+        self.driver = Driver().phonedriver()
+        self.driver.maximize_window()
+        loginpage = MbLoginPage(self.driver)
+        loginpage.user_login('https://qy.do1.com.cn/qiqiao/mruntime', account, password)
+        time.sleep(5)
+
+
+
+
+    def test_01( self ):
+        '''【补丁】PC运行平台--多表关联组件配置只读，数据列表不显示外键字段值;  移动端--点击编辑进入表单中间表字段值都不显示'''
+        self.mbLogin("wujianlun@auto","do1qiqiao")
+        homePage = MbHomePage(self.driver)
+        homePage.HomePage_BottomNav_Click("待办")
+        # 发起流程
+        todoPage = MbTodoPage(self.driver)
+        todoPage.MbTodoPage_Faqiliucheng('HSE管理','防护用品申请')
+        formPage = MbFormPage(self.driver)
+
+        formPage.MbSelection_SingleXiala_Senkeys("申请原因","到期更换")
+        formPage.MbText_Sendkeys("原因说明","很大很大空间等哈看进度哈大噶还记得噶还记得噶实践活动")
+        formPage.MbSelection_SingleBox_Senkeys("检索类别","个人PPE")
+        time.sleep(2)
+        formPage.MbMultiForm_BathManagePage_Record_Tick()
+        formPage.MultiForm_BatchManagementButton_Click("用品明细")
+        formPage.MbMultiForm_BathManagePage_Record_Tick("用品明细", [1])
+        formPage.MultiForm_sendkeysTo_Number("用品明细",1,"申请数量",20)
+        formPage.MultiForm_BathManagePage_ConfirmButton_Tick("用品明细")
+        #检查多表关联组件数据列表显示
+        self.assertEqual("口罩",formPage.MbMultiForm_GetTdValue("用品明细",1,2),msg="用品明细用品名称显示不正确")
+        self.assertEqual("个人PPE",formPage.MbMultiForm_GetTdValue("用品明细",1,3),msg="用品明细类别显示不正确")
+        self.assertEqual("50",formPage.MbMultiForm_GetTdValue("用品明细",1,4),msg="用品明细库存显示不正确")
+        self.assertEqual("道一",formPage.MbMultiForm_GetTdValue("用品明细",1,5),msg="用品明细单位显示不正确")
+        self.assertEqual("20",formPage.MbMultiForm_GetTdValue("用品明细",1,6),msg="用品明细申请数量显示不正确")
+        #点击多表关联组件编辑按钮
+        formPage.MultiForm_edit_Record("用品明细",1)
+        #检查表单弹窗内的数据显示
+        self.assertEqual("口罩",formPage.ForeignSelection_GetValue_writable_InPopup("用品明细","用品名称"),msg="用品明细用品名称显示不正确")
+        self.assertEqual("个人PPE",formPage.Selection_SingleXiala_readOnly_InPopup("用品明细","类别"),msg="用品明细类别显示不正确")
+        self.assertEqual("50",formPage.Number_GetValue_readOnly_InPopup("用品明细","库存"),msg="用品明细库存显示不正确")
+        self.assertEqual("道一",formPage.Text_GetValue_readOnly_InPopup("用品明细","单位"),msg="用品明细单位显示不正确")
+        self.assertEqual(20,formPage.Number_GetValue_writable_InPopup("用品明细","申请数量"),msg="用品明细申请数量显示不正确")
+
+        formPage.Form_Close_Popup("用品明细")
+        time.sleep(1)
+        formPage.Form_Button_Click("提交")
+        formPage.Form_ProcessHandle_Pop_QuerenButton_Click()  # 点击流程办理弹框确认按钮
+        self.assertIn('成功',formPage.Public_GetAlertMessage(),msg="第一个人工任务办理失败")
+        time.sleep(5)
+
+        # 进行第二个人工任务处理
+        portalPage.PortalPage_Click_HeaderMenu('流程')
+        processPage.ProcessPage_click_process_menu("我的待办")
+        processPage.ProcessPage_click_process_record(1)
+        self.assertEqual("口罩",formPage.MbMultiForm_GetTdValue("用品明细",1,2),msg="用品明细用品名称显示不正确")
+        self.assertEqual("个人PPE",formPage.MbMultiForm_GetTdValue("用品明细",1,3),msg="用品明细类别显示不正确")
+        self.assertEqual("50",formPage.MbMultiForm_GetTdValue("用品明细",1,4),msg="用品明细库存显示不正确")
+        self.assertEqual("道一",formPage.MbMultiForm_GetTdValue("用品明细",1,5),msg="用品明细单位显示不正确")
+        self.assertEqual("20",formPage.MbMultiForm_GetTdValue("用品明细",1,6),msg="用品明细申请数量显示不正确")
+        formPage.Form_Button_Click("办理")
+        formPage.Form_ProcessHandle_Pop_QuerenButton_Click()  # 点击流程办理弹框确认按钮
+        self.assertIn('成功',formPage.Public_GetAlertMessage(),msg="第二个人工任务办理失败")
+        
+        # 进行第三个人工任务处理
+        portalPage.PortalPage_Click_HeaderMenu('流程')
+        processPage.ProcessPage_click_process_menu("我的待办")
+        processPage.ProcessPage_click_process_record(1)
+        self.assertEqual("口罩",formPage.MbMultiForm_GetTdValue("用品明细",1,2),msg="用品明细用品名称显示不正确")
+        self.assertEqual("个人PPE",formPage.MbMultiForm_GetTdValue("用品明细",1,3),msg="用品明细类别显示不正确")
+        self.assertEqual("50",formPage.MbMultiForm_GetTdValue("用品明细",1,4),msg="用品明细库存显示不正确")
+        self.assertEqual("道一",formPage.MbMultiForm_GetTdValue("用品明细",1,5),msg="用品明细单位显示不正确")
+        self.assertEqual("20",formPage.MbMultiForm_GetTdValue("用品明细",1,6),msg="用品明细申请数量显示不正确")
+        formPage.Form_Button_Click("办理")
+        formPage.Form_ProcessHandle_Pop_QuerenButton_Click()  # 点击流程办理弹框确认按钮
+        self.assertIn('成功',formPage.Public_GetAlertMessage(),msg="第三个人工任务办理失败")
+        
+        
+        # 进行第四个人工任务处理
+        portalPage.PortalPage_Click_HeaderMenu('流程')
+        processPage.ProcessPage_click_process_menu("我的待办")
+        processPage.ProcessPage_click_process_record(1)
+        self.assertEqual("口罩",formPage.MbMultiForm_GetTdValue("用品明细",1,2),msg="用品明细用品名称显示不正确")
+        self.assertEqual("个人PPE",formPage.MbMultiForm_GetTdValue("用品明细",1,3),msg="用品明细类别显示不正确")
+        self.assertEqual("50",formPage.MbMultiForm_GetTdValue("用品明细",1,4),msg="用品明细库存显示不正确")
+        self.assertEqual("道一",formPage.MbMultiForm_GetTdValue("用品明细",1,5),msg="用品明细单位显示不正确")
+        self.assertEqual("20",formPage.MbMultiForm_GetTdValue("用品明细",1,6),msg="用品明细申请数量显示不正确")
+        #
+        formPage.MultiForm_Click_Row("用品明细",1)
+        #检查表单弹窗内的数据显示
+        self.assertEqual("口罩",formPage.ForeignSelection_GetValue_readOnly_InPopup("用品明细","用品名称"),msg="用品明细用品名称显示不正确")
+        self.assertEqual("个人PPE",formPage.Selection_SingleXiala_readOnly_InPopup("用品明细","类别"),msg="用品明细类别显示不正确")
+        self.assertEqual("50",formPage.Number_GetValue_readOnly_InPopup("用品明细","库存"),msg="用品明细库存显示不正确")
+        self.assertEqual("道一",formPage.Text_GetValue_readOnly_InPopup("用品明细","单位"),msg="用品明细单位显示不正确")
+        self.assertEqual("20",formPage.Number_GetValue_readOnly_InPopup("用品明细","申请数量"),msg="用品明细申请数量显示不正确")
+        formPage.Form_Close_Popup("用品明细")
+        time.sleep(1)
+        formPage.Form_Button_Click("办理")
+        formPage.Form_ProcessHandle_Pop_QuerenButton_Click()  # 点击流程办理弹框确认按钮
+        self.assertIn('成功',formPage.Public_GetAlertMessage(),msg="第四个人工任务办理失败")
